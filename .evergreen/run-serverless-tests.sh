25e5fa15e06d49401d59039418a572a44ea77a2f
Neal Beeken <neal.beeken@mongodb.com>
2021-11-29 13:23:05 -0500

    wip

--- .evergreen/run-serverless-tests.sh
+++ .evergreen/run-serverless-tests.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-source "${PROJECT_DIRECTORY}/.evergreen/install-dependencies.sh"
+# source "${PROJECT_DIRECTORY}/.evergreen/install-dependencies.sh"
 
 if [ -z ${SERVERLESS+omitted} ]; then echo "SERVERLESS is unset" && exit 1; fi
 if [ -z ${MULTI_ATLASPROXY_SERVERLESS_URI+omitted} ]; then echo "MULTI_ATLASPROXY_SERVERLESS_URI is unset" && exit 1; fi
 if [ -z ${SINGLE_ATLASPROXY_SERVERLESS_URI+omitted} ]; then echo "SINGLE_ATLASPROXY_SERVERLESS_URI is unset" && exit 1; fi
-if [ -z ${SINGLE_MONGOS_LB_URI+omitted} ]; then echo "SINGLE_MONGOS_LB_URI is unset" && exit 1; fi
-if [ -z ${MULTI_MONGOS_LB_URI+omitted} ]; then echo "MULTI_MONGOS_LB_URI is unset" && exit 1; fi
+# if [ -z ${SINGLE_MONGOS_LB_URI+omitted} ]; then echo "SINGLE_MONGOS_LB_URI is unset" && exit 1; fi
+# if [ -z ${MULTI_MONGOS_LB_URI+omitted} ]; then echo "MULTI_MONGOS_LB_URI is unset" && exit 1; fi
 if [ -z ${MONGODB_URI+omitted} ]; then echo "MONGODB_URI is unset" && exit 1; fi
 if [ -z ${SERVERLESS_ATLAS_USER+omitted} ]; then echo "SERVERLESS_ATLAS_USER is unset" && exit 1; fi
 if [ -z ${SERVERLESS_ATLAS_PASSWORD+omitted} ]; then echo "SERVERLESS_ATLAS_PASSWORD is unset" && exit 1; fi
 
-npx mocha --file test/tools/runner/index.js \
-  test/functional/crud_spec.test.js \
-  test/functional/retryable_reads.test.js \
-  test/functional/retryable_writes.test.js \
-  test/functional/sessions.test.js \
-  test/functional/transactions.test.js \
-  test/functional/versioned-api.test.js \
-  test/functional/load-balancer-spec.test.js
+npx mocha --file test/tools/runner/index.js test/functional/transactions.test.js
+  # test/functional/crud_spec.test.js \
+  # test/functional/retryable_reads.test.js \
+  # test/functional/retryable_writes.test.js \
+  # test/functional/sessions.test.js \
+  # test/functional/transactions.test.js \
+  # test/functional/versioned-api.test.js \
+  # test/functional/load-balancer-spec.test.js
